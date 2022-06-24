5ec579fd7ae073546346a26c84a0fc20168c8952
Isabel Queiroz <belqrdev@outlook.com>
2022-06-24 18:34:24 -0300

    Atividade OK!

---
+++ index.js
@@ -0,0 +1,30 @@
+//Questão 1:
+
+let gatos = ['Pandora', 'Mel', 'Pipoca', 'Salém', 'Luna'];
+console.log('Questão 1:', gatos);
+
+//Questão 2:
+
+gatos.unshift('Theo');
+console.log('Questão 2:', gatos);
+
+//Questão 3:
+
+gatos.pop();
+console.log('Questão 3:', gatos);
+
+//Questão 4:
+
+gatos.push('Luna', 'Nina');
+console.log('Questão 4:', gatos);
+
+//Questão 5:
+
+gatos.shift();
+console.log('Questão 5:', gatos);
+
+//Questão 6:
+
+let numbers = [7,5,6,3,8,9,2,1,4];
+numbers.sort();
+console.log('Questão 6:', numbers);
